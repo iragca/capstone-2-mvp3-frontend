9187e8e759841d00daf0e31ca5fc2aebfac51a37
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-09-14 13:49:03 +0800

    fix: improve error handling in inference actions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from './$types';
 import type { Actions } from './$types';
 import type { InferenceRequest } from '$lib/types';
+import { fail } from '@sveltejs/kit';
 
 export const load = (async () => {
 	return {};
@@ -16,8 +17,19 @@ export const actions = {
 
 		const endpoint = 'http://localhost:8001/inference';
 
+		function basicError(message: string) {
+			return {
+				success: false,
+				error: message,
+				username,
+				userId,
+				topK,
+				strictUsername
+			};
+		}
+
 		if (username.length === 0) {
-			return { success: false, error: 'Username is required' };
+			return fail(400, basicError('Username is required'));
 		}
 
 		const body: InferenceRequest = {
@@ -29,23 +41,25 @@ export const actions = {
 			}
 		};
 
-		console.log('Request Body:', body);
+		try {
+			const response = await fetch(endpoint, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(body)
+			});
 
-		const response = await fetch(endpoint, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(body)
-		});
+			if (!response.ok) {
+				console.error('Error:', response.statusText);
+				return { success: false, error: response.statusText };
+			}
 
-		if (!response.ok) {
-			console.error('Error:', response.statusText);
-			return { success: false, error: response.statusText };
+			const data = await response.json();
+			return { success: true, data: data.data };
+		} catch (error) {
+			console.error('Fetch error:', error);
+			return fail(500, basicError('Internal Server Error'));
 		}
-
-		const data = await response.json();
-		console.log('Success:', data);
-		return { success: true, data: data };
 	}
 } satisfies Actions;
